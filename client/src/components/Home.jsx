8b408118e8c50b64346a3e27367e28850579a68c
Amar Patel <amarpatel@amars-air.myfiosgateway.com>
2021-04-02 15:41:05 -0400

    working on homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,18 @@
 import "./Home.css";
+import MainPic from "../images/long road ahead.jpeg";
 
 function Home(props) {
   return (
     <div className="home">
-      <h1>Home Goes Here</h1>
+      <h1>Welcome to Travel Keeper</h1>
+      <div className="home-banner-image">
+        <img src={MainPic} alt="main" />
+        <div className="headliner">
+          This a place for the adventurers, the travelers, and the
+          experimenters. Send us your dreams and we'll help you share them with
+          the world.
+        </div>
+      </div>
     </div>
   );
 }
--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,12 +1,13 @@
 import Nav from "./Nav";
 import Footer from "./Footer";
+import "./Layout.css";
 
 function Layout(props) {
   const { currentUser, handleLogout } = props;
   return (
-    <div>
+    <div className="layout">
       <Nav currentUser={currentUser} handleLogout={handleLogout} />
-      {props.children}
+      <div className="props-children">{props.children}</div>
       <Footer />
     </div>
   );
